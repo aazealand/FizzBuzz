77eba9725cdcd742f7c5d58a2952106bc17edae0
Andrew Ziemba <aazealand@gmail.com>
2015-12-27 20:19:34 -0500

    Test2

--- FizzBuzz/DetailViewController.swift
+++ FizzBuzz/DetailViewController.swift
@@ -13,7 +13,7 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var detailDescriptionLabel: UILabel!
 
     //comments!!!!!!!!!
-
+    //comments galore....
     var detailItem: AnyObject? {
         didSet {
             // Update the view.
